cf5b6cd23c444f9be58d7ca209b77d2e4405d465
Dan Heintzelman <heintzelman@gmail.com>
2016-06-08 14:53:52 -0500

    add reccuring payment functionality

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,18 +4,43 @@ class ChargesController < ApplicationController
   def new
     @total_cost = current_user.cart_total
     @token = Braintree::ClientToken.generate
+    if current_user.customer_id
+      render 'repeat'
+    else
+      render 'new'
+    end
   end
 
   def create
     @total_cost = current_user.cart_total
     nonce = params[:payment_method_nonce]
+
+    # If payment was method was submitted, create a customer
+    if nonce
+      customer_result = Braintree::Customer.create(
+        first_name: current_user.first_name,
+        last_name: current_user.last_name,
+        payment_method_nonce: nonce
+      )
+      # If customer/method are submitted without error, store customer ID for repeat payments
+      if customer_result.success?
+        puts customer_result.customer
+        current_user.update(customer_id: customer_result.customer.id)
+      else
+        @errors = customer_result.errors
+        render 'new'
+      end
+    end
+    # Either customer ID was just created or it already existed. Now we can process transaction.
+    customer_id = current_user.customer_id
     result = Braintree::Transaction.sale(
-      amount: "#{@total_cost}",
-      payment_method_nonce: nonce,
+    customer_id: customer_id,
+    amount: "#{@total_cost}",
       options: {
         submit_for_settlement: true
       }
     )
+    puts result
     if result.success?
       redirect_to complete_charge_path(result: "Success")
     else
@@ -31,5 +56,7 @@ class ChargesController < ApplicationController
       @error = "Transaction failed :("
     end
   end
+
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160605192014) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "password_digest"
+    t.string   "customer_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
